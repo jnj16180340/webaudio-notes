56c4febc29f9798414f010624b1f5f3f5608d950
nate johnson <nate@biobright.org>
2017-02-20 17:51:18 -0500

    Refactoring; osc chirp works

--- webaudio-demo.js
+++ webaudio-demo.js
@@ -7,6 +7,7 @@ const useWebsocket = true; // send data to server
 const bufferSize = 4096; // for ScriptProcessor nodes
 
 var sources = []; // all the audio sources that will need to be start()-ed
+var sourceNode; // microphone, oscillator, ...
 var intermediateNode; // this is the node in the middle of the filter graph
 
 // If this was in production, we would check for AudioContext support here
@@ -14,48 +15,46 @@ var audioContext;
 audioContext = new AudioContext();
 
 // noise function
-function myPCMFilterFunction(inputSample) {
+function myPCMFilterFunction(inputSample){
   var noiseSample = Math.random() * 2 - 1;
   return inputSample + noiseSample * 0.1;  // For example, add noise samples.
 }
 
 // Create a pcm processing "node" for the filter graph, this is a dummy that adds a little bit of noise
-var addNoiseNode = audioContext.createScriptProcessor(bufferSize, 1, 1);
-addNoiseNode.onaudioprocess = function(e) {
+let addNoiseNode = audioContext.createScriptProcessor(bufferSize, 1, 1);
+addNoiseNode.onaudioprocess = function(e){
   // this just ignores any extra channels... okay
   var input = e.inputBuffer.getChannelData(0);
   var output = e.outputBuffer.getChannelData(0);
-  for (var i = 0; i < bufferSize; i++) {
+  for (var i = 0; i < bufferSize; i++){
     // Modify the input and send it to the output.
     output[i] = myPCMFilterFunction(input[i]);
   }
 }
 
-var teeStreamNode;
 if(useWebsocket){
-  // is this in global scope...
-  // WRONG LOCATION
-  // should be defined in a node.
+  // connect to server
   var socket = new WebSocket('ws://localhost:8080');
   
-  socket.addEventListener('open', function (event) {
+  socket.addEventListener('open', function (event){
     console.log('Connected to server!');
+    // This is where we could negotiate formats etc.
     //socket.send('Hello Server!');
   });
   
-  //socket.addEventListener('message', function (event) {
+  //socket.addEventListener('message', function (event){
   //  console.log('Message from server', event.data);
   //});
   
   let teeStreamNode = audioContext.createScriptProcessor(bufferSize, 1, 1);
-  teeStreamNode.onaudioprocess = function(e) {
+  teeStreamNode.onaudioprocess = function(e){
     // this just ignores any extra channels... okay
     // does this node make webaudio downmix everything?
     var input = e.inputBuffer.getChannelData(0);
     var output = e.outputBuffer.getChannelData(0);
     // might need to copy to an intermediate typedarray before streaming out
     //output = input; // doesn't work, we need to copy values explicitly
-    for (var i = 0; i < bufferSize; i++) {
+    for (var i = 0; i < bufferSize; i++){
       // Modify the input and send it to the output.
       output[i] = myPCMFilterFunction(input[i]);
     }
@@ -72,53 +71,56 @@ var errorCallback = function(err){
 
 
 if(useMicrophone){
-  var mediaConstraints = {audio: true}; 
+  let mediaConstraints = {audio: true}; 
   // new-style promise based getUserMedia
   navigator.mediaDevices.getUserMedia(mediaConstraints)
   .then(function(stream){
     // microphone -> addNoiseNode -> destination.
-    var microphone = audioContext.createMediaStreamSource(stream);
-    microphone.connect(intermediateNode);
+    let microphone = audioContext.createMediaStreamSource(stream);
+    sourceNode = microphone;
+    sources.push(sourceNode);
+    //microphone.connect(intermediateNode);
     // destination of audioContext is set automatically (in this case)
-    intermediateNode.connect(audioContext.destination);
-    //microphone.start(0);
+    //intermediateNode.connect(audioContext.destination);
+    //microphone.start();
   })
   .catch(errorCallback);
 }
 
 
 if(useOscillator){
-  // do crap
-  var osc = audioContext.createOscillator();
+  let osc = audioContext.createOscillator();
   // we could replace this with new OscillatorNode(params)
   osc.type = 'sine';
   osc.frequency.value = 440;
   osc.detune = 0;
-  // osc.setPeriodicWave(piff) // for wavetable synthesis
   
-  //osc.connect(addNoiseNode);
-  //addNoiseNode.connect(audioContext.destination);
-  osc.connect(intermediateNode);
-  intermediateNode.connect(audioContext.destination);
+  //osc.connect(intermediateNode);
+  //intermediateNode.connect(audioContext.destination);
   
   // Frequency sweep 0...880Hz
   
   // WebAudio automation does not work inside setTimeout events
-  //osc.frequency.linearRampToValueAtTime(880,2);
+  //e.g. osc.frequency.linearRampToValueAtTime(880,2);
   
   // We can connect oscillator node TO A PARAMETER?!
-  var lfo = audioContext.createOscillator();
+  let lfo = audioContext.createOscillator();
   lfo.type='sawtooth';
   lfo.frequency.value = 0.2;
   // Oscillators go from -1 to 1; scale its output
-  var lfoGain = audioContext.createGain();
+  let lfoGain = audioContext.createGain();
   lfoGain.gain.value = 440;
   lfo.connect(lfoGain);
   lfoGain.connect(osc.frequency);
   
+  sourceNode = osc;
+  
+  sources.push(lfo);
+  sources.push(sourceNode);
+  
   // ALL SOURCES MUST BE STARTED
-  lfo.start(); // must be called
-  osc.start(); // must be called
+  //lfo.start(); // must be called
+  //osc.start(); // must be called
 }
 
 if(useMonotonic){
@@ -126,10 +128,13 @@ if(useMonotonic){
 }
 
 if(useConstant){
-  throw new Error('constant audio not implemented yet');
+  throw new Error('constant "audio" not implemented yet');
 }
 
 // Build the filter graph, start things, etc.
 // We assume topology: Source -> ScriptProcessorNode -> Destination
-
-
+sourceNode.connect(intermediateNode);
+intermediateNode.connect(audioContext.destination);
+for(i in sources){
+  sources[i].start();
+}
