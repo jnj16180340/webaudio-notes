5f41ed8e05a33615ece9a4334780a3454abfec55
nate johnson <nate@biobright.org>
2017-02-21 17:34:35 -0500

    Set up server as module for running as worker or standalone

--- server.js
+++ server.js
@@ -1,61 +1,72 @@
+#!/usr/bin/env node
 'use strict';
 
 // server: consumes binary stream and writes it to a file.
 
-const WebSocket = require('ws');
-const fs = require('fs'); // don't really need to import this tho...
-const exec = require('child_process').exec;
+function run(){
+  const WebSocket = require('ws');
+  const fs = require('fs'); // don't really need to import this tho...
+  const exec = require('child_process').exec;
 
-// use websocket compression "ONLY if REALLY needed", overhead in memory+performance
-const wss = new WebSocket.Server({
-  perMessageDeflate: false,
-  port: 8080
-});
-
-wss.on('connection', function connection(ws) {
-  // If we wanted to communicate audio info (channels, bitrate etc) we would do it here
-  // For now, we assume 44.1kHz/float32/mono
-  // Realistically, only sample rate may change unexpectedly
-  // because sample rate is automatically set by client based on *everything*
-  // however client may query sample rate and send it.
-  console.log('New connection');
-  let filenamebase = Date.now().toString();
-  let filename = `${filenamebase}.raw`; // ms timestamp
-  
-  // Character encoding is important. fs defaults to utf-8
-  let outstream = fs.createWriteStream(filename, {flags:'w'});
-  
-  outstream.on('open', function(){
-    console.log(`Opened file ${filename}`);
-  })
-  outstream.on('close', function(){
-    console.log(`Closed file ${filename}`);
-  })
-  
-  ws.on('message', function incoming(message){
-    //console.log('received: %s', message);
-    outstream.write(message);
+  // use websocket compression "ONLY if REALLY needed", overhead in memory+performance
+  const wss = new WebSocket.Server({
+    perMessageDeflate: false,
+    port: 8080
   });
-  
-  ws.on('close', function close(){
-    console.log('Closed connection!');
-    // WAV header is written here, because it contains total length
-    outstream.end(); // this flushes buffer before closing file
+
+  wss.on('connection', function connection(ws) {
+    // If we wanted to communicate audio info (channels, bitrate etc) we would do it here
+    // For now, we assume 44.1kHz/float32/mono
+    // Realistically, only sample rate may change unexpectedly
+    // because sample rate is automatically set by client based on *everything*
+    // however client may query sample rate and send it.
+    console.log('New connection');
+    let filenamebase = Date.now().toString();
+    let filename = `${filenamebase}.raw`; // ms timestamp
     
-    // WAV conversion is just for convenience.
-    let conversion_command = `sox -r 44100 -e floating-point -b 32 -c 1 -t raw ${filename} ${filenamebase}.wav`;
-    exec(conversion_command, function(err, stdout, stderr){
-      console.log('Converting RAW to WAV');
-      if (err) {
-        console.error(`exec error: ${error}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
+    // Character encoding is important. fs defaults to utf-8
+    let outstream = fs.createWriteStream(filename, {flags:'w'});
+    
+    outstream.on('open', function(){
+      console.log(`Opened file ${filename}`);
     })
-  })
-  //setInterval(function(){ws.send('ping!')},2000)
-});
+    outstream.on('close', function(){
+      console.log(`Closed file ${filename}`);
+    })
+    
+    ws.on('message', function incoming(message){
+      //console.log('received: %s', message);
+      outstream.write(message);
+    });
+    
+    ws.on('close', function close(){
+      console.log('Closed connection!');
+      // WAV header is written here, because it contains total length
+      outstream.end(); // this flushes buffer before closing file
+      
+      // WAV conversion is just for convenience.
+      let conversion_command = `sox -r 44100 -e floating-point -b 32 -c 1 -t raw ${filename} ${filenamebase}.wav`;
+      exec(conversion_command, function(err, stdout, stderr){
+        console.log('Converting RAW to WAV');
+        if (err) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+      })
+    })
+    //setInterval(function(){ws.send('ping!')},2000)
+  });
+}
+
+if(require.main === module){
+  // if this is run directly from command line.
+  // if called as a worker, we'll call run() directly
+  run()
+}
+
+module.exports.run = run;
 
 // On client:
 /*
